43499bba4aa8933c55ff89bf56e3dc6c4d6e1ad4
Confidence360 <confidenceibeh361@gmail.com>
2025-05-21 02:32:16 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('BLACKOUT app loaded');
+let deferredPrompt;
+const installButton = document.getElementById('install-button');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', async () => {
+    installButton.style.display = 'none';
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to install prompt: ${outcome}`);
+    deferredPrompt = null;
+  });
+});
