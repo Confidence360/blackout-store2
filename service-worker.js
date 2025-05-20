e04d6386cb7608ea49a5b1f1d21415db26110053
Confidence360 <confidenceibeh361@gmail.com>
2025-05-21 02:45:32 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,45 @@
+const CACHE_NAME = 'blackout-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon.png',
+  '/icon-512.png'
+];
+
+// Install the service worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('blackout-cache').then(cache => {
-      return cache.addAll([
-        'index.html',
-        'style.css',
-        'app.js',
-        'manifest.json',
-        'icon.png'
-      ]);
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate the service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
 
+// Fetch resources
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
