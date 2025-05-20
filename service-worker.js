c5af4206834bcfea37cbffd90c573466035158ec
Confidence360 <confidenceibeh361@gmail.com>
2025-05-21 03:29:37 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,32 @@
-git add service-worker.js
-git commit -m "Fix: updated service worker to fix blank screen issue"
-git push origin main
+const CACHE_NAME = 'blackout-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon.png',
+  '/products.csv'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(filesToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    })))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
